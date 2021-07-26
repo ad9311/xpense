3ece085ea6c21a7312c6a1b9192784e19c528764
Ángel Díaz <diazangel9311@gmail.com>
2021-07-26 15:17:19 -0500

    Add round to 2 decimal place for expenses.amount

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,6 @@ class User < ApplicationRecord
   has_many :folders, class_name: 'Group', foreign_key: 'user_id'
 
   def total_spent
-    expenses.map(&:amount).inject(:+)
+    expenses.map(&:amount).inject(:+).round(2)
   end
 end
