ea36b947634fd59924aecbce03d9fecc351e7463
Ángel Díaz <diazangel9311@gmail.com>
2021-09-05 17:37:25 -0500

    Improve menu when scrolling
    Remove unnecessary div and class

--- app/javascript/packs/menu.js
+++ app/javascript/packs/menu.js
@@ -1,5 +1,4 @@
-function* toggle() {
-  const menu = document.getElementById('menu');
+function* toggle(menu) {
   while (true) {
     menu.classList += ' menu-show';
     yield 1
@@ -8,6 +7,13 @@ function* toggle() {
   }
 }
 
+const close = (menu, toggle) => {
+  toggle.next();
+  menu.classList = 'menu';
+}
+
+const menu = document.getElementById('menu');
 const ham = document.getElementById('ham');
-const gen = toggle();
+const gen = toggle(menu);
+
 ham.addEventListener('click', () => gen.next());
