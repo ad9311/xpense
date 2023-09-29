e734c1e2174679050ead7419e4f4f88ca053c8be
Ángel Díaz <diazangel9311@gmail.com>
2023-09-29 10:02:12 -0500

    Remove registratable module from user

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,8 +27,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :trackable
+  devise :database_authenticatable, :recoverable, :rememberable, :validatable, :trackable
 
   validates :first_name, :last_name, presence: true
 
