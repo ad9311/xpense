bc81913371377c295803f251be8d0e1bd1f7299c
Ángel Díaz <diazangel9311@gmail.com>
2023-09-29 00:05:40 -0500

    Bind rails to 0.0.0.0:3000

--- config/puma.rb
+++ config/puma.rb
@@ -47,7 +47,6 @@ threads threads_count, threads_count
 preload_app!
 
 rackup      DefaultRackup
-port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
 bind        'tcp://0.0.0.0:3000' # Specify the desired host and port
 plugin :tmp_restart
