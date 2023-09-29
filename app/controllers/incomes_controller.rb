a4e9e3064abf5800f3cd3b9e3412c2bfe7d157ad
Ángel Díaz <diazangel9311@gmail.com>
2023-09-28 22:06:33 -0500

    Implement edit and delete income and expenses

--- app/controllers/incomes_controller.rb
+++ app/controllers/incomes_controller.rb
@@ -1,8 +1,12 @@
 class IncomesController < ApplicationController
   before_action :set_cycle
+  before_action :set_income, only: %i[edit update destroy]
+
   def new
     @income = Income.new
   end
+
+  def edit; end
 
   def create
     @income = @cycle.incomes.build(income_params)
@@ -13,10 +17,30 @@ class IncomesController < ApplicationController
     end
   end
 
+  def update
+    if @income.update(income_params)
+      redirect_to cycle_path(@cycle)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @income.destroy
+      redirect_to cycle_path(@cycle)
+    else
+      render :edit
+    end
+  end
+
   private
 
   def set_cycle
     @cycle = Cycle.find_by(id: params[:cycle_id])
+  end
+
+  def set_income
+    @income = Income.find_by(id: params[:id])
   end
 
   def income_params
--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,11 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources 'cycles', only: %i[show edit] do
-    resources 'incomes', only: %i[new create]
-    resources 'expenses', only: %i[new create]
+    resources 'incomes', only: %i[new create edit update destroy]
+    resources 'expenses', only: %i[new create edit update destroy]
   end
   patch 'cycles/:id/edit_expense_limit', to: 'cycles#update_expense_limit'
+
 
   resources 'users', only: %i[show]
   resources 'fixed_transactions', only: %i[new create]
